e3872dd61e68bb961198342fb6ab49b524cb85d6
Alfonso Harita <alfonso@ifooddecisionsciences.com>
2021-12-07 22:01:24 -0700

    day 7

---
+++ day7/part1.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# https://adventofcode.com/2021/day/7
+
+data = []
+minimum_fuel = 0
+
+File.open('input.txt').each.with_index do |line, _line_index|
+  data = line.split(',').map(&:to_i)
+end
+
+data.each do |d|
+  fuel = 0
+
+  data.each do |position|
+    fuel += (position - d).abs
+  end
+
+  minimum_fuel = fuel if minimum_fuel.zero?
+  minimum_fuel = [minimum_fuel, fuel].min
+end
+
+puts minimum_fuel
