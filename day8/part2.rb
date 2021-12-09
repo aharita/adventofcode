373a41917d78ca6895ad54dd274cd9a6983399f9
Alfonso Harita <alfonso@ifooddecisionsciences.com>
2021-12-08 22:44:31 -0700

    indents

--- day8/part2.rb
+++ day8/part2.rb
@@ -14,21 +14,27 @@ File.open('input.txt').each.with_index do |line, _line_index|
   numbers[8] = data.find { |x| x.length == 7 }
 
   candidates = data.select { |x| x.length == 5 }
-  number_3_index = candidates.map { |x| x.gsub(/[#{numbers[7]}]/, '') }.find_index { |x| x.length == 2 }
+  number_3_index = candidates.map { |x| x.gsub(/[#{numbers[7]}]/, '') }
+                             .find_index { |x| x.length == 2 }
   numbers[3] = candidates[number_3_index]
 
   candidates -= [numbers[3]]
-  number_5_index = candidates.map { |x| x.gsub(/[#{numbers[7]}]/, '') }.map { |x| x.gsub(/[#{numbers[4]}]/, '') }.find_index { |x| x.length == 1 }
+  number_5_index = candidates.map { |x| x.gsub(/[#{numbers[7]}]/, '') }
+                             .map { |x| x.gsub(/[#{numbers[4]}]/, '') }
+                             .find_index { |x| x.length == 1 }
   numbers[5] = candidates[number_5_index]
 
   numbers[2] = (candidates - [numbers[5]]).first
 
   candidates = data.select { |x| x.length == 6 }
-  number_9_index = candidates.map { |x| x.gsub(/[#{numbers[1]}]/, '') }.map { |x| x.gsub(/[#{numbers[5]}]/, '') }.find_index { |x| x.length == 0 }
+  number_9_index = candidates.map { |x| x.gsub(/[#{numbers[1]}]/, '') }
+                             .map { |x| x.gsub(/[#{numbers[5]}]/, '') }
+                             .find_index { |x| x.length == 0 }
   numbers[9] = candidates[number_9_index]
 
   candidates -= [numbers[9]]
-  number_0_index = candidates.map { |x| x.gsub(/[#{numbers[1]}]/, '') }.find_index { |x| x.length == 4 }
+  number_0_index = candidates.map { |x| x.gsub(/[#{numbers[1]}]/, '') }
+                             .find_index { |x| x.length == 4 }
   numbers[0] = candidates[number_0_index]
 
   numbers[6] = (candidates - [numbers[0]]).first
