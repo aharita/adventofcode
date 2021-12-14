b3abebab9861e9479bf37feb6585c4eac08a8aa8
Alfonso Harita <alfonso@ifooddecisionsciences.com>
2021-12-13 22:48:26 -0700

    day9 recursion

--- day9/part2.rb
+++ day9/part2.rb
@@ -2,25 +2,35 @@
 
 # https://adventofcode.com/2021/day/9
 
-# data = [[10] * 102]
-# File.open('input.txt').each.with_index do |line, _line_index|
-#   line = line.strip.split('').map(&:to_i)
-#   data << ([10].concat(line).append(10))
-# end
-# data << ([10] * 102)
+seen = {}
+sizes = []
+data = [[9] * 102]
+File.open('input.txt').each.with_index do |line, _line_index|
+  line = line.strip.split('').map(&:to_i)
+  data << ([9].concat(line).append(9))
+end
+data << ([9] * 102)
 
-# sum = 0
-# (1..data.length - 2).each do |row_index|
-#   (1..data[row_index].length - 2).each do |col_index|
-#     current = data[row_index][col_index]
+def check_point(x, y, data, seen)
+  key = "#{x},#{y}"
+  return 0 if seen[key] == true
 
-#     next if current >= data[row_index][col_index - 1]
-#     next if current >= data[row_index][col_index + 1]
-#     next if current >= data[row_index - 1][col_index]
-#     next if current >= data[row_index + 1][col_index]
+  seen[key] = true
+  return 0 if x > 101 || y > 101 || data[x][y] == 9
 
-#     sum += current + 1
-#   end
-# end
+  sum = 0
+  sum += check_point(x + 1, y, data, seen)
+  sum += check_point(x - 1, y, data, seen)
+  sum += check_point(x, y + 1, data, seen)
+  sum += check_point(x, y - 1, data, seen)
 
-# puts sum
+  return sum + 1
+end
+
+(1..data.length - 2).each do |row_index|
+  (1..data[row_index].length - 2).each do |col_index|
+    sizes << check_point(row_index, col_index, data, seen)
+  end
+end
+
+puts sizes.sort.last(3).inject(&:*)
