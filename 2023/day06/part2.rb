1a3adc10e7b29284578676f3eedb6626f252a1d1
Alfonso Harita <alfonso@ifoodds.com>
2023-12-06 08:58:20 -0700

    day6

---
+++ 2023/day06/part2.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+# https://adventofcode.com/2023/day/6
+
+data = []
+File.open('input.txt').each do |line|
+  data << line.strip
+end
+
+times = data[0].split(':')[1].join(' ').map(&:to_i)
+distances = data[1].split(':')[1].join(' ').map(&:to_i)
+
+puts times.to_s
+puts distances.to_s
+
+wins = Array.new(times.length, 0)
+times.each_with_index do |time, index|
+  # Get middle
+  middle = time / 2
+  
+  hold = middle
+  remaining = time - middle
+  distance = hold * remaining
+  if distance > distances[index]
+    wins[index] += 1
+  end
+  
+  # Walk left
+  while true
+    middle = middle - 1
+    hold = middle
+    remaining = time - middle
+    distance = hold * remaining
+    if distance > distances[index]
+      wins[index] += 1
+    else
+      break
+    end
+  end
+
+  middle = time / 2
+  
+  # Walk right
+  while true
+    middle = middle + 1
+    hold = middle
+    remaining = time - middle
+    distance = hold * remaining
+    if distance > distances[index]
+      wins[index] += 1
+    else
+      break
+    end
+  end
+end
+
+puts wins.inject(:*)
